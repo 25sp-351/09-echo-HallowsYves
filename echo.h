d6edded4589eb89df0ab1f31c17cb3c75aa34f22
= <vyves246@gmail.com>
2025-04-14 15:16:32 -0700

    work

--- echo.h
+++ echo.h
@@ -2,8 +2,8 @@
 #define ECHO_H
 
 
-
-
+int DEFAULT_PORT;
+int VERBOSE;
 void handleConnection(int a_client);
 
 
--- handleConnection.c
+++ handleConnection.c
@@ -8,5 +8,27 @@
 
 
 void handleConnection(int a_client) {
+    int socket_fd = createSocket(DEFAULT_PORT);
+
+    // Read input 
+    char buffer[1024];
+    ssize_t bytes_read = read(socket_fd, buffer, sizeof(buffer));
+    char temp_buffer[1024];
+    int line_index = 0;
+
+    
+    // Loop through the buffer to see where "\n" is
+    for (int index = 0; index < bytes_read; index++) {
+
+
+        // Store values in temp_buffer 
+        temp_buffer[line_index++] = buffer[index];
+
+        if (buffer[index] == '\n') {
+            // Once we get '\n' we send it back to the client, and reset line Index, 
+            temp_buffer[0] = buffer[index];
+        }
+
+    }
 
 }
